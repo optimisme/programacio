480c1a735f1a44c3f3009db7a84264abcae76769
Alber <optimisme@gmail.com>
2020-10-21 10:08:36 +0200

    Fix ex004 (3)

--- exercici004.js
+++ exercici004.js
@@ -8,7 +8,7 @@ al executar-lo sigui:
     Quant diners et deu en Marc? 523
     Quant diners li vols perdonar? 23
     El deute d'en Marc és de  523 €
-    Després de perdonar-li 23 , ara el deute és de  500
+    Després de perdonar-li 23, ara el deute és de  500
 
 On 523 i 23 són valors introduïts per l'usuari
 
@@ -24,7 +24,7 @@ async function main () {
     // Resta aquí el valor de 'condonament' al deute d'en Marc
     
 
-    // Mostra aquí el text 'Després de perdonar-li XX , ara el deute és de  YY'
+    // Mostra aquí el text 'Després de perdonar-li XX, ara el deute és de  YY'
     // on XX és el valor de condonament
     // i YY és el valor final del deute
 }
