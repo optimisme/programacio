15ecbe37755ff00e41458dcd4d76a3410bac5272
Alber <optimisme@gmail.com>
2020-10-21 10:03:52 +0200

    Fix ex004

--- exercici004.js
+++ exercici004.js
@@ -11,12 +11,13 @@ al executar-lo sigui:
 async function main () {
 
     let deute = 1500
+    let condonament = parseFloat(await prompt("Quant diners li vols perdonar? "))
 
     console.log('El deute d\'en Marc és de ', deute, '€')
     
-    // Resta aquí 250€ al deute d'en Marc
+    // Resta aquí el valor de 'condonament' al deute d'en Marc
 
-    console.log('Després de perdonar-li una part, ara el deute és de ', deute)
+    console.log('Després de perdonar-li', condonament,', ara el deute és de ', deute)
 }
 
 main()
