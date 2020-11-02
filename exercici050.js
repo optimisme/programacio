6390213837061d785896107b7554e73218898491
Alber <optimisme@gmail.com>
2020-11-02 12:38:16 +0100

    Exercici 50

---
+++ exercici050.js
@@ -0,0 +1,37 @@
+const prompt = require('prompt-promise');
+
+/*
+
+Fes un programa que demani d'escriure una frase i:
+- mostri la llargada de cada paraula de la frase (ignorant 'en', 'la', 'i', 'al', 'a', 'el')
+- digui si la primera lletra és vocal o consonant
+- escrigui la última lletra de cada paraula
+
+Per exemple:
+
+    Escriu una frase: la Vero i en Toni han anat al taller a buscar el cotxe
+    Vero:
+      llarg = 4,lletres (V, o), la primera lletra és consonant
+    Toni:
+      llarg = 4,lletres (T, i), la primera lletra és consonant
+    han:
+      llarg = 3,lletres (h, n), la primera lletra és consonant
+    anat:
+      llarg = 4,lletres (a, t), la primera lletra és vocal
+    taller:
+      llarg = 6,lletres (t, r), la primera lletra és consonant
+    buscar:
+      llarg = 6,lletres (b, r), la primera lletra és consonant
+    cotxe:
+      llarg = 5,lletres (c, e), la primera lletra és consonant
+
+*/
+
+async function main () {
+
+    // Posa el codi necessari aquí
+
+    prompt.end()
+}
+
+main()
