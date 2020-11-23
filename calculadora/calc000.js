8c367296e25968133af8a0999a266d428e4f7f2c
Albert <optimisme@gmail.com>
2020-11-23 09:54:37 +0100

    Afegit exercici 'calculadora

---
+++ calculadora/calc000.js
@@ -0,0 +1,28 @@
+
+
+let refDisplay = null
+
+function init() {
+
+    // TODO: Agafa la referència a l'objecte amb id 'display'
+    //       i guarda aquesta referència a la variable 'refDisplay'
+}
+
+// TODO: Crea la funció 'afegeix(valor)'
+//       aquesta funció ha d'afegir el valor
+//       que rep com a paràmetre al final
+//       de l'input anomenat 'display'
+
+// TODO: Crea la funció 'borrar' sense paràmetres
+//       aquesta funció a d'esborrar el contingut
+//       de l'input anomenat 'display'
+
+// TODO: Crea la funció 'calcular' sense paràmetres
+//       aquesta funció ha de calcular el valor
+//       de  l'input anomenat 'display'
+//       fent servir un 'eval', 
+//       dins d'una estructura 'try/catch', aleshores:
+//       - Si no hi ha hagut error, apunta
+//         el resultat a l'input anomenat 'display'
+//       - Si hi ha hagut error, escriu 'Error'
+//         a l'input anomenat 'display'
