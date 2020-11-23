f8cb3473256457b16fc81e8d3b89b556c3af2455
Albert <optimisme@gmail.com>
2020-11-23 17:03:30 +0100

    Nou exercici 'contador'

---
+++ contador/contador000.js
@@ -0,0 +1,17 @@
+
+// Recorda que a les següents funcions, et farà falta
+// una variable 'refContador' que faci referència
+// a l'objecte amb id 'contador'
+
+// TODO: Crea la funció 'add()'
+//       aquesta funció ha de sumar un 1
+//       al valor que hi hagi al div amb id 'contador'
+//       i guardar el resultat al mateix div 'contador'
+//
+//       hauràs d'agafar el valor amb '.innerHTML'
+//       recorda que això és un 'text' que has de transformar
+//       a número amb 'parseFloat'
+//
+//       modifica el codi HTML perquè
+//       el botó cridi a aquesta funció 'add()' a través
+//       de un 'onclick'
